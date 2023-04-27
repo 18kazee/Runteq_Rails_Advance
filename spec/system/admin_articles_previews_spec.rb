61cd6723199f94360c89751deef31b17a91dd040
18kazee <18kazee2415@gmail.com>
2023-04-28 01:12:21 +0900

    Add: システムスペックの追加

--- spec/system/admin_articles_previews_spec.rb
+++ spec/system/admin_articles_previews_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'AdimnArticlesPreview', type: :system do
   let(:admin) { create :user, :admin }
+  before do
+    login(admin)
+  end
   describe '記事作成画面で画像ブロックを追加' do
     context '画像を添付せずにプレビューを閲覧' do
       it '正常に表示される' do
-        login(admin)
         click_link '記事'
         visit admin_articles_path
         click_link '新規作成'
@@ -17,6 +19,20 @@ RSpec.describe 'AdimnArticlesPreview', type: :system do
         click_link 'プレビュー'
         switch_to_window(windows.last)
         expect(page).to have_content('テスト記事'), 'プレビューページが正しく表示されていません'
+      end
+    end
+  end
+  describe '記事作成画面で文章ブロックブロックを追加' do
+    let!(:article) { create :article }
+    context '文章を追加せずにプレビューを閲覧' do
+      it '正常に表示される' do
+        visit edit_admin_article_path(article.uuid)
+        click_link 'ブロックを追加する'
+        click_link '文章'
+        click_link 'プレビュー'
+        switch_to_window(windows.last)
+        expect(page).to have_content(article.title), 'プレビューページが正しく表示されていません'
+        expect(page).not_to have_content('no implict conversion of nill into String:'), 'プレビューページがエラー画面になっています'
       end
     end
   end
--- spec/system/admin_tags_spec.rb
+++ spec/system/admin_tags_spec.rb
@@ -2,14 +2,42 @@ require 'rails_helper'
 
 RSpec.describe "adminTags", type: :system do
   let(:admin) { create :user, :admin }
-  let(:tag) { create :tag }
-
-  it 'タグのパンくずリストが機能すること' do
+  before do
     login(admin)
-    visit edit_admin_tag_path(tag)
-    within('.breadcrumb') do
-      click_link 'タグ'
+  end
+  describe 'タグ一覧画面' do
+    it 'Home > タグというパンくずが表示されていること' do
+      visit admin_tags_path
+      within('.breadcrumb') do
+        expect(page).to have_content('Home'), 'Homeというパンくずが表示されていません'
+        expect(page).to have_content('タグ'), '「タグ」というパンくずが表示されていません'
+      end
     end
-    expect(current_path).to eq(admin_tags_path), 'パンくずのタグを押した時にダッシュボードに遷移していません'
+    it '「Home」のパンくずをクリックした時にダッシュボード画面に遷移すること' do
+      visit admin_tags_path
+      within('.breadcrumb') do
+        click_link 'Home'
+      end
+      expect(current_path).to eq(admin_dashboard_path), 'パンくずのHomeを押した時ダッシュボードに遷移していません'
+    end
+  end
+
+  describe 'タグ編集画面' do
+    let!(:tag) { create :tag }
+    it 'Home > タグ > タグ編集というパンくずが表示されていること' do
+      visit edit_admin_tag_path(tag)
+      within('.breadcrumb') do
+        expect(page).to have_content('Home'), 'Homeというパンくずが表示されていません'
+        expect(page).to have_content('タグ'), '「タグ」というパンくずが表示されていません'
+        expect(page).to have_content('タグ編集'), '「タグ編集」というパンくずが表示されていません'
+      end
+    end
+    it '「タグ」のパンくずをクリックしたときにタグの一覧画面に遷移すること' do
+      visit edit_admin_tag_path(tag)
+      within('.breadcrumb') do
+        click_link 'タグ'
+      end
+      expect(current_path).to eq(admin_tags_path), 'パンくずの「タグ」を押した時にタグ一覧に画面遷移していません'
+    end
   end
 end
