cde6280fdd2c67b7e5549487c1f23ffe46997194
18kazee <18kazee2415@gmail.com>
2023-05-20 07:44:39 +0900

    Fix: Lint修正

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,9 +1,8 @@
 class ArticleMailer < ApplicationMailer
-  def report_summary 
+  def report_summary
     @published_count = Article.published.count
     @article_published_yesterday = Article.published_at_yesterday
 
     mail(to: 'admin@example.com', subject: '公開済記事の集計結果')
   end
 end
-
