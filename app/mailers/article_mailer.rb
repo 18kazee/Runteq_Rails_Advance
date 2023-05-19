ca70305a441c7734b3d397deb3ea60435f78775c
18kazee <18kazee2415@gmail.com>
2023-05-20 01:33:03 +0900

    Add: メール本文とメソッドを追加

---
+++ app/mailers/article_mailer.rb
@@ -0,0 +1,9 @@
+class ArticleMailer < ApplicationMailer
+  def report_summary 
+    @published_count = Article.published.count
+    @article_published_yesterday = Article.published_at_yesterday
+
+    mail(to: 'admin@example.com', subject: '公開済記事の集計結果')
+  end
+end
+
